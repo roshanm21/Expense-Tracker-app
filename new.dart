dac7017c9ab7ef2d02bd6e421e1989f4f6a229ae
roshanm21 <roshworkspace01@gmail.com>
2026-09-15 12:08:43 +0530

    Dart Basics

---
+++ new.dart
@@ -0,0 +1,15 @@
+void baiscs() {
+  print('hello');
+  List arr = [
+    'ID323',
+    [3, 4, 2],
+    'boy',
+    33
+  ];
+  print(arr[1][2]);
+  List<List> arrr = [[1,2,3]];
+}
+
+void main(){
+  
+}
