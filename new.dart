32a8b99531ecd88dd5dc3e9398b2dcd27a92a204
roshanm21 <roshworkspace01@gmail.com>
2026-09-20 18:28:01 +0530

    20 September Notes

--- new.dart
+++ new.dart
@@ -7,9 +7,33 @@ void baiscs() {
     33
   ];
   print(arr[1][2]);
-  List<List> arrr = [[1,2,3]];
+  List<List> arrr = [
+    [1, 2, 3]
+  ];
+  print(arrr);
+}
+//=========>
+
+//Class (blueprint)
+class User {
+  
+
+  String username;
+  int age;
+
+  User(String a, int b) {
+    username = a;
+    age = b;
+  }
+  void login() {
+    print("user logged in");
+  }
 }
 
-void main(){
-  
+void main() {
+  User userone = User('mario',12);
+  print(userone.username);
+
+  User usertwo = User('bario', 32);
+  print(usertwo.username);
 }
